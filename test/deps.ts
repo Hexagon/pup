4c00a43b241d4a085bf711739d5c0bae6783dfca
Hexagon <hexagon@56k.guru>
2023-11-22 20:47:02 +0100

    Update dependencies. Prepare 1.0.0-rc.12.

--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export { assert, assertEquals, assertGreater, assertNotEquals, assertRejects, assertThrows } from "https://deno.land/std@0.204.0/assert/mod.ts"
-export { assertSpyCall, spy } from "https://deno.land/std@0.204.0/testing/mock.ts"
+export { assert, assertEquals, assertGreater, assertNotEquals, assertRejects, assertThrows } from "https://deno.land/std@0.207.0/assert/mod.ts"
+export { assertSpyCall, spy } from "https://deno.land/std@0.207.0/testing/mock.ts"
