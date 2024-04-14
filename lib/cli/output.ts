0475743af8a00f6b34280f03e8aec6353989c626
Hexagon <hexagon@56k.guru>
2024-04-14 20:45:59 +0200

    Fix typo

--- lib/cli/output.ts
+++ lib/cli/output.ts
@@ -29,7 +29,7 @@ export function createFlagsMessage(externalInstaller: boolean): string {
     { separator: "empty" },
     { content: "Control and monitor instances", spanStart: 1 },
     { separator: "empty" },
-    { long: "run", description: "Run a pup instance in run" },
+    { long: "run", description: "Run a pup instance standalone" },
     { long: "terminate", description: "Terminate pup instance using IPC" },
     { long: "status", description: "Show status for a pup instance" },
     { separator: "empty" },
